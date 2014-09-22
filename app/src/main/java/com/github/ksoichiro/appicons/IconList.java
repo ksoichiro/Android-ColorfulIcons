a3b33f78ffa2a42f634098498d33827709efbbc0
ksoichiro <soichiro.kashima@gmail.com>
2014-09-23 00:50:11 +0900

    Added other type of window(s).

--- app/src/main/java/com/github/ksoichiro/appicons/IconList.java
+++ app/src/main/java/com/github/ksoichiro/appicons/IconList.java
@@ -62,6 +62,7 @@ public class IconList extends ArrayList<Icon> {
         add(new Icon(R.drawable.ic_action_redo, "ic_action_redo"));
         add(new Icon(R.drawable.ic_action_document, "ic_action_document"));
         add(new Icon(R.drawable.ic_action_windows, "ic_action_windows"));
+        add(new Icon(R.drawable.ic_action_windows2, "ic_action_windows2"));
     }
 
 }
